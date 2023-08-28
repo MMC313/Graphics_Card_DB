1a744a29de58f68df56ee08093246e8f13f820df
MartianMooCow <NathenL2832@gmail.com>
2023-08-28 16:11:02 -0400

    added content section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
+import Content from './Components/Content/Content'
 import './App.css'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
   return(
     <>
       <Header/>
+      <Content/>
     </>
   )
 }
