f8f379b3f7a2f633d5e6887833e109f209e6d4ca
MartianMooCow <NathenL2832@gmail.com>
2023-08-31 15:39:59 -0400

    added backend connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,30 @@ import Header from './Components/Header/Header'
 import Content from './Components/Content/Content'
 import './App.css'
 
+
+
 export default function App() {
+
+  const baseUrl ='http://localhost:3000/results'
+  const options= {
+      method: 'GET'
+  }
+
+  async function loadInfo(searchInput){  
+    let res = await fetch(baseUrl + new URLSearchParams({search:searchInput}),options)
+    let data = await res.json()
+
+    console.log(data.documents[0])
+    return data.documents[0]
+  }
+
+  const [headerQuery,setHeaderQuery] = useState("donkey")
+
 
   return(
     <>
-      <Header/>
-      <Content/>
+      <Header setQuery={setHeaderQuery}/>
+      <Content query={headerQuery}/>
     </>
   )
 }
