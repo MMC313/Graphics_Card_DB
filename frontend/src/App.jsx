013ceb5427c1d2ea2bdc6764d117e456f29f737a
MartianMooCow <NathenL2832@gmail.com>
2023-09-05 16:39:22 -0400

    added '?' to the end of baseURL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,31 +2,46 @@ import { useState } from 'react'
 import Header from './Components/Header/Header'
 import Content from './Components/Content/Content'
 import './App.css'
+import { useEffect } from 'react'
 
 
 
 export default function App() {
+  
 
-  const baseUrl ='http://localhost:3000/results'
+  const baseUrl ='http://localhost:3000/results?'
   const options= {
       method: 'GET'
   }
 
+  const [headerQuery,setHeaderQuery] = useState("")
+  const [gpuData,setGpuData] = useState("")
+
   async function loadInfo(searchInput){  
     let res = await fetch(baseUrl + new URLSearchParams({search:searchInput}),options)
     let data = await res.json()
-
-    console.log(data.documents[0])
-    return data.documents[0]
+    // let res = await fetch(baseUrl,options)
+    // let data = await res.json()
+    setGpuData(data.documents[0])
+   
+    // return data.documents[0]
   }
 
-  const [headerQuery,setHeaderQuery] = useState("donkey")
+  async function loadSpecs(){
+    // let apiData = await loadInfo(headerQuery)
+    // setGpuData(apiData)
+  }
 
+  useEffect(()=>{
+    loadInfo(headerQuery)
+  },[headerQuery])
+
+  console.log("App rerender")
 
   return(
     <>
       <Header setQuery={setHeaderQuery}/>
-      <Content query={headerQuery}/>
+      <Content data={gpuData}/>
     </>
   )
 }
