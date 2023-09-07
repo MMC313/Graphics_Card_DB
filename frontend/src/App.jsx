15f2de84171757c37eb9bc312d57d4e31a1fd623
MartianMooCow <NathenL2832@gmail.com>
2023-09-07 16:20:39 -0400

    relocated where the loadinfo function was called into the components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,40 +8,14 @@ import { useEffect } from 'react'
 
 export default function App() {
   
-
-  const baseUrl ='http://localhost:3000/results?'
-  const options= {
-      method: 'GET'
-  }
+  console.log("App rerender")
 
   const [headerQuery,setHeaderQuery] = useState("")
-  const [gpuData,setGpuData] = useState("")
-
-  async function loadInfo(searchInput){  
-    let res = await fetch(baseUrl + new URLSearchParams({search:searchInput}),options)
-    let data = await res.json()
-    // let res = await fetch(baseUrl,options)
-    // let data = await res.json()
-    setGpuData(data.documents[0])
-   
-    // return data.documents[0]
-  }
-
-  async function loadSpecs(){
-    // let apiData = await loadInfo(headerQuery)
-    // setGpuData(apiData)
-  }
-
-  useEffect(()=>{
-    loadInfo(headerQuery)
-  },[headerQuery])
-
-  console.log("App rerender")
 
   return(
     <>
       <Header setQuery={setHeaderQuery}/>
-      <Content data={gpuData}/>
+      <Content headerQuery={headerQuery}/>
     </>
   )
 }
